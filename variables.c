08c5c18298ebafded0b3841b7e44f126ac1caf93
Grant Davis <Grant@zada.local>
2020-02-27 05:40:56 -0800

    Variable types def, correction

---
+++ variables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*Variables exist for C by types, and are considered higher hierearchy of the language*/
+
+int main()
+{
+  printf("decimal number: %d",55);
+  printf("floating pointer number: %f",5.555);
+  printf("All variables: %f, %d, %d, 3.33, 33, 3");
+  printf("intergers whole number: %d",55);
+  printf("char characters: %c",a,b,c,d,f);
+  printf("strings: %s", "Helloworld\n");
+  return 0;
+}
