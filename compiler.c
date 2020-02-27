fe679926da7b341e9b9f75bc21b24ca54d445cb6
Grant Davis <Grant@zada.local>
2020-02-27 05:25:39 -0800

    Compiler explanations

---
+++ compiler.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*Explanations for compiler - clang/gcc*/
+int main()
+{
+  printf("Ther are four stages to compilation\n");
+  printf("Preprocessor should and can be done when source code written\n");
+  printf("Compiler accepts preprocess code and begins the assembler language");
+  printf("Compiler passes parsed assembled syntax to the assembler");
+  printf("The assembler passes code to linker for standard library\n ");
+  return 0;
+}
