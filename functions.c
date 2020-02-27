dc0a62e4c512a8287326164fa7a3776e7f6b1cc5
Grant Davis <Grant@zada.local>
2020-02-27 05:53:12 -0800

    Define functions

---
+++ functions.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+/*Functions are stmts to do are keep doing a line or block of code to reduce continuos jobs*/
+int main()
+{
+  printf(" Printf prints to the screen\n");
+  return 0;
+}
