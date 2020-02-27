1fc69c8bc2795c69897f43a2788fd9952b603137
Grant Davis <Grant@zada.local>
2020-02-27 05:01:30 -0800

    Standardization

---
+++ library.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* This standard was added for standard output. <stdio.h> is used for printing.*/
+int main()
+/*begin wrapper*/
+{
+
+  /*Manprintf()*/
+
+  printf("Print f provides formated routine\n");
+
+}
+ 
